f3ec4c35c434c1988c66ecf0b0c8f4054ad17112
Manolo Carrasco <manolo@vaadin.com>
2025-10-23 08:22:40 +0200

    do not use VaadinWebSecurity API but the new VaadinAwareSecurityContextHolderStrategyConfiguration

--- src/main/java/com/vaadin/starter/bakery/app/security/PerformanceTestSecurityConfiguration.java
+++ src/main/java/com/vaadin/starter/bakery/app/security/PerformanceTestSecurityConfiguration.java
@@ -9,14 +9,17 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.SecurityFilterChain;
 
-import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import com.vaadin.flow.spring.security.VaadinAwareSecurityContextHolderStrategyConfiguration;
 import com.vaadin.starter.bakery.backend.data.Role;
 
 
@@ -27,11 +30,12 @@ import com.vaadin.starter.bakery.backend.data.Role;
 @Configuration
 @Order(1)
 @Profile("performance-test")
-public class PerformanceTestSecurityConfiguration extends VaadinWebSecurity {
+@Import(VaadinAwareSecurityContextHolderStrategyConfiguration.class)
+public class PerformanceTestSecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
+    @Bean
+    public SecurityFilterChain performanceSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
             // Not using Spring CSRF here to be able to use plain HTML for the login page
             .csrf(csrf -> csrf.disable())
 
@@ -47,7 +51,7 @@ public class PerformanceTestSecurityConfiguration extends VaadinWebSecurity {
                 .authorities(Arrays.stream(Role.getAllRoles())
                     .map(SimpleGrantedAuthority::new)
                     .collect(Collectors.toList()))
-            );
+            ).build();
     }
 
 }
